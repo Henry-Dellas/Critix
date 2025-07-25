a7eec9ba2f1254c450f874b84f7b8d638359dfee
Pixo <lucascardclaudino@gmail.com>
2025-07-25 13:27:10 -0300

    leve correção
    UMA DAS IMAGENS TÁ APARECENDO AGR, dps vo atrás da outra

--- imagem.php
+++ imagem.php
@@ -1,15 +1,18 @@
 <?php
 $pdo = new PDO("pgsql:host=localhost;dbname=bancox", "postgres", "System@2025");
 $id = $_GET['id'] ?? 0;
+
 $stmt = $pdo->prepare("SELECT imagem FROM filmes WHERE id = ?");
 $stmt->execute([$id]);
-$filme = $stmt->fetch(PDO::FETCH_ASSOC);
-if ($filme && !empty($filme['imagem'])) {
+$stmt->bindColumn('imagem', $imagem, PDO::PARAM_LOB);
+$stmt->fetch(PDO::FETCH_BOUND);
+
+if (!empty($imagem)) {
     header("Content-Type: image/jpeg");
-    echo $filme['imagem'];
+    fpassthru($imagem); 
     exit;
 } else {
     header("Content-Type: text/plain");
     echo "Imagem não encontrada.";
+    exit;
 }
-?>
